2d6f05620bb6b1786fce378e39ab50238f552576
Subham Paul <subhampaul1694@gmail.com>
2025-01-10 21:22:45 +0530

    Initial project set up with Ts and tailwind

---
+++ fooddelivery/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-red-400">
+      <h1 className="text-4xl font-bold text-blue-500">
+        React + TypeScript + Tailwind CSS
+      </h1>
+    </div>
+  );
+}
+
+export default App;
